bc9d1743e0e32625228b2c9fe10dd8084f588a4d
Woems <0190666666@freenet.de>
2013-03-11 15:23:44 +0100

    gcr: geht

--- bash/git-commit-reminder.sh
+++ bash/git-commit-reminder.sh
@@ -1,3 +1,43 @@
+
 #!/bin/bash
 
+GitArchive="$HOME/.mozilla/firefox/azua9l03.default/gm_scripts"
+ScriptRun="./cleanup.sh"
+Ignor="config"
 
+while true
+do
+  cd "$GitArchive"
+  pwd
+  date
+  AnzMod=$(git status | grep "modified" | grep -v "$Ignor" | wc -l)
+  if [ $AnzMod -ge 1 ]; then
+    echo "MODIFIED"
+    $(sleep 1 && wmctrl -a Frage -b add,above)&
+    RET=$(zenity --question --text "$AnzMod Dateien wurden modifiziert. Commiten?")
+    if [ $? -eq 0 ]; then
+      if git status | grep "modified:.*config.xml" > /dev/null; then
+        xmlstarlet ed -L -d "/UserScriptConfig/Script/@installTime" -d "/UserScriptConfig/Script/@lastUpdateCheck" -d "/UserScriptConfig/Script/@modified" -d "/UserScriptConfig/Script/@uuid" config.xml
+        git add config.xml ; git commit -m "config"
+      fi
+      echo "## git gui"
+      git gui
+      echo "--RET: $?"
+      echo "## git pull"
+      git pull
+      echo "--RET: $?"
+      echo "## git mergetool -y"
+      git mergetool -y
+      echo "--RET: $?"
+      echo "## git commit -m 'Merge'"
+      git commit -m "Merge"
+      echo "--RET: $?"
+      echo "## git push -u origin master"
+      git push -u origin master
+      echo "--RET: $?"
+    fi
+  fi
+  echo "Mod: $AnzMod"
+  sleep 600
+  # sec => 1h=60*60=3600
+done
